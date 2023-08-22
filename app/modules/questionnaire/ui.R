381a07a8fa7d7df3f3f43d0e5e9c6dc7fa0214b1
Sian Dennett <dennettsian@gmail.com>
2023-08-22 18:23:10 +0100

    More formattin changes. Added bookmark button

--- app/modules/questionnaire/ui.R
+++ app/modules/questionnaire/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(rlang)
 
 ui <- shiny::fluidPage(
 
@@ -13,24 +14,26 @@ ui <- shiny::fluidPage(
                        label = "What type of drug is being tested?",
                        choices = list("Chemotherapy",
                                       "Immunotherapy",
-                                      "Targeted Agent")),
+                                      "Targeted Agent"),
+                       width = 500),
 
     shiny::radioButtons("know_doses",
                         label = "Do you know how many dose levels are being
                         tested?",
                         choices = list("No", "Yes"),
-                        selected = "No"),
+                        selected = "No",
+                        width = 500),
 
     shiny::conditionalPanel(
       "input.know_doses == 'Yes'",
       shiny::sliderInput("n_doses",
                          label = "How many dose levels are being tested?",
-                         value = 5, min = 1, max = 10)
+                         value = 5, min = 1, max = 10, width = 500)
     ),
 
     shiny::numericInput("start_dose",
                         label = "What is the starting dose level?",
-                        value = 1, min = 1, max = 10),
+                        value = 1, min = 1, max = 10, width = 500),
 
     shiny::radioButtons("know_prior_tox_info",
                         label = "How much prior knowledge of the drug's toxicity
@@ -39,69 +42,71 @@ ui <- shiny::fluidPage(
                                        "Some pre-clinical information",
                                        "Lots of pre-clinical information",
                                        "Some clinical information",
-                                       "Lots of clinical information")),
+                                       "Lots of clinical information"),
+                        width = 500),
 
     shiny::radioButtons("know_ttl",
                         label = "Do you know the target toxicity level for your
                         trial?",
                         choices = list("No", "Yes"),
-                        selected = "No"),
+                        selected = "No", width = 500),
 
     shiny::conditionalPanel(
       "input.know_ttl == 'Yes'",
       shiny::sliderInput("ttl",
                          label = "What is the target toxicity level for
                          this trial?",
-                         value = 0.2, min = 0, max = 1, step = 0.05)
+                         value = 0.2, min = 0, max = 1, step = 0.05,
+                         width = 500)
     ),
 
     shiny::radioButtons("need_tox_interval",
                         label = "Do you want to provide an indifference interval
                         in which you are happy for the toxicity rate of the
                         recommended dose to fall?",
-                        choices = list("No", "Yes")),
+                        choices = list("No", "Yes"), width = 500),
 
     shiny::radioButtons("stats_help",
                         label = "Are you able and willing to provide real time
                         data to a statistician for analysis during the trial?",
-                        choices = list("No", "Yes")),
+                        choices = list("No", "Yes")), width = 500,
 
     shiny::radioButtons("know_late_tox",
                         label = "Do you need to consider late toxicities
                         occurring in your trial?",
-                        choices = list("No", "Yes")),
+                        choices = list("No", "Yes"), width = 500),
 
     shiny::radioButtons("cohort_vary",
                         label = "Do you want to have varying cohort sizes?",
                         choices = list("No", "Yes"),
-                        selected = "No"),
+                        selected = "No", width = 500),
 
     shiny::conditionalPanel(
       "input.cohort_vary == 'No'",
       shiny::sliderInput("cohort_size",
                          label = "What size will the cohorts be?",
                          value = 3, min = 1, max = 6,
-                         step = 1, ticks = FALSE)
+                         step = 1, ticks = FALSE, width = 500)
     ),
 
     shiny::radioButtons("know_max_n",
                         label = "Is there a maximum sample size for
                         this trial?",
                         choices = list("No", "Yes"),
-                        selected = "No"),
+                        selected = "No", width = 500),
 
     shiny::conditionalPanel(
       "input.know_max_n == 'Yes'",
       shiny::numericInput("max_n",
                           label = "What is the maximum sample
                           size for this trial?",
-                          value = 1, min = 1, max = 1000)
+                          value = 1, min = 1, max = 1000, width = 500)
     ),
 
     shiny::fluidRow(
       actionButton("get_rating",
                    label = "Generate recommendations!",
-                   class = "btn-block")
+                   width = 500)
     )
   ),
 
@@ -109,11 +114,8 @@ ui <- shiny::fluidPage(
   # recommended method, for now.
   textOutput("recommendations"),
 
+  bookmarkButton()
+
 )
 
-
-server <- function(input, output, session) {
-}
-
-
-shinyApp(ui, server)
+shinyApp(ui, server, enableBookmarking = "server")
