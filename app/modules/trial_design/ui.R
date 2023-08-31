d9dfa0f4a2e3ff09d0927ceda040e5de9650fb29
Eduardo <eduardomelloviegas@gmail.com>
2023-08-31 14:37:53 +0100

    Made code robust to rankings of any length

--- app/modules/trial_design/ui.R
+++ app/modules/trial_design/ui.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(escalation)
+library(trialr)
+library(ggplot2)
 
 
 
@@ -6,128 +9,144 @@ library(shiny)
 
 #1.0 DEFINING VARIABLES TO BE INPUTTED FROM QUESTIONNAIRE UI
 
-#Most variable names were taken from the master_variables spreadsheet published by Sian and reviewed by Jake, added to main branch on 22Aug23
-#Variable names not taken from spreadsheet are signalled below
-#Variable values are arbitrary since we have not integrated them from the Questionnaire UI yet; I tried to stick to the defaults specified in the spreadsheet
+#Variables/parameters are defined individually here as Sian is working on unifying them into a database
+#I should eventually be able to pull from database to send to backend modelling (Jake)
+#Variables such as n_sims should be sent to Jake in list format
 
-##user_profile variables:
-ranking <- c("3p3", "crm", "other") #Not taken from spreadhseet; first ranking possibility. Commen/uncomment and re-run script
-#ranking <- c("crm", "3p3", "other") #Second ranking possibility. Commen/uncomment and re-run script
-#ranking <- c("other", "3p3", "crm") #Third ranking possibility. Commen/uncomment and re-run script
+##1.1 user_profile variables:
+ranking <- c("tpt", "crm", "other") #Comment/uncomment and re-run to check UI
+##ranking <- c("other", "tpt", "crm")
+##ranking <- c("tpt")
+##ranking <- c("tpt", "crm")
+##ranking <- c("crm", "tpt")
+##ranking <- c("tpt", "crm", "other", "other2")
+##ranking <- c("other2", "tpt", "crm", "other")
 
-##non_spec variables (non design-specific):
+##1.2 non_spec variables (non-design-specific):
 n_doses <- 5
 start_dose <- 1
 cohort_size <- 3
 ttl <- 0.2
 max_n <- 24
 curent_seed <- 1234
-#Maybe the other simulation parameters (n_sims, true_dlt_ss) should be here
+##Perhaps simulation parameters (true_dlt_ss, n_sims) should be in this section
 
-##spec_3p3 variables (trial-design-specific, particularly 3 plus3):
+##1.3 spec_tpt variables (design-specific, particularly 3+3):
 tpt_allow_deesc <- FALSE
-true_dlt_ss_3p3_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6) #Not taken from spreadsheet
-true_dlt_ss_3p3_2 <- c(0, 0.1, 0.2, 0.3, 0.4) #Not taken from spreadsheet
-true_dlt_ss_3p3_3 <- c(0, 0, 0, 0.1, 0.2) #Not taken from spreadsheet
-n_sims_3p3 <- 20 #Not taken from spreadsheet
+true_dlt_ss_tpt_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6)
+true_dlt_ss_tpt_2 <- c(0, 0.1, 0.2, 0.3, 0.4)
+true_dlt_ss_tpt_3 <- c(0, 0, 0, 0.1, 0.2)
+n_sims_tpt <- 20
 
-##spec_crm variables (trial-design-specific, particularly CRM):
+##1.4 spec_crm variables (design-specific, particularly CRM):
 prior_mtd <- 3
 prior_ttp <- c(0.02, 0.08, 0.20, 0.36, 0.52)
 prior_var <- 0.5
 skip_esc <- FALSE
+dont_skip_esc <- TRUE #Defining this because if fits better as a get_dfcrm() argument
 skip_deesc <- TRUE
+dont_skip_deesc <- FALSE #Defining this because if fits better as a get_dfcrm() argument
 no_esc_if_observed_gt_target <- TRUE
 stop_tox_x <- 0.1
 stop_tox_y <- 0.7
-stop_n_mtd <- 12 #12 default not taken from spreadsheet because 25 seemed too high
-true_dlt_ss_crm_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6) #Not taken from spreadsheet
-true_dlt_ss_crm_2 <- c(0, 0.1, 0.2, 0.3, 0.4) #Not taken from spreadsheet
-true_dlt_ss_crm_3 <- c(0, 0, 0, 0.1, 0.2) #Not taken from spreadsheet
-n_sims_crm <- 20 #Not taken from spreadsheet
+stop_n_mtd <- 12 #12 default value not taken from spreadsheet because 25 seemed too high
+true_dlt_ss_crm_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6)
+true_dlt_ss_crm_2 <- c(0, 0.1, 0.2, 0.3, 0.4)
+true_dlt_ss_crm_3 <- c(0, 0, 0, 0.1, 0.2)
+n_sims_crm <- 20
 
-##spec_other variables (trial-design-specific, particularly another design):
-mock_parameter <- 1 #Not taken from spreadsheet
-true_dlt_ss_other_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6) #Not taken from spreadsheet
-true_dlt_ss_other_2 <- c(0, 0.1, 0.2, 0.3, 0.4) #Not taken from spreadsheet
-true_dlt_ss_other_3 <- c(0, 0, 0, 0.1, 0.2) #Not taken from spreadsheet
-n_sims_other <- 20 #Not taken from spreadsheet
+##1.5 spec_other variables (design-specific, particularly "other" design):
+other_allow_deesc <- FALSE
+true_dlt_ss_other_1 <- c(0.2, 0.3, 0.4, 0.5, 0.6)
+true_dlt_ss_other_2 <- c(0, 0.1, 0.2, 0.3, 0.4)
+true_dlt_ss_other_3 <- c(0, 0, 0, 0.1, 0.2)
+n_sims_other <- 20
 
-# #Joining some of the parameters into lists; this may be useful to send to modelling backend (Jake)
+##1.6 spec_other2 variables (design-specific, particularly "other2" design):
+##This is just so I can test whether my code is robust to more than 3 recommended designs
+mock_parameter <- "Test, test, test"
 
-# true_dlt_ss_3p3_list <- list(
-#   true_dlt_ss_3p3_1,
-#   true_dlt_ss_3p3_2,
-#   true_dlt_ss_3p3_3
-# )
-# true_dlt_ss_crm_list <- list(
-#   true_dlt_ss_crm_1,
-#   true_dlt_ss_crm_2,
-#   true_dlt_ss_crm_3
-# )
-# true_dlt_ss_other_list <- list(
-#   true_dlt_ss_other_1,
-#   true_dlt_ss_other_2,
-#   true_dlt_ss_other_3
-# )
-# true_dlt_ss_all_list <- list(
-#   true_dlt_ss_3p3_list,
-#   true_dlt_ss_crm_list,
-#   true_dlt_ss_other_list
-# )
-# names(true_dlt_ss_all_list) <- c("3p3", "crm", "other")
-# true_dlt_ss_all_list
-# true_dlt_ss_all_list$'3p3'
-# true_dlt_ss_all_list$crm
-# true_dlt_ss_all_list$other
+##1.7 Joining some of the variables into lists to be sent to Jake, and testing they can be indexed
+##Not much of a point in coding this now since variables will eventually be pulled from database (?)
 
-# n_sims_all_list <- list(
-#   n_sims_3p3,
-#   n_sims_crm,
-#   n_sims_other
-# )
-# names(n_sims_all_list) <- c("3p3", "crm", "other")
-# n_sims_all_list
-# n_sims_all_list$'3p3'
-# n_sims_all_list$crm
-# n_sims_all_list$other
+###1.7.1 Simulation scenario true DLT rates
+true_dlt_ss_tpt_list <- list(
+  true_dlt_ss_tpt_1,
+  true_dlt_ss_tpt_2,
+  true_dlt_ss_tpt_3
+)
+true_dlt_ss_crm_list <- list(
+  true_dlt_ss_crm_1,
+  true_dlt_ss_crm_2,
+  true_dlt_ss_crm_3
+)
+true_dlt_ss_other_list <- list(
+  true_dlt_ss_other_1,
+  true_dlt_ss_other_2,
+  true_dlt_ss_other_3
+)
+true_dlt_ss_all_list <- list(
+  true_dlt_ss_tpt_list,
+  true_dlt_ss_crm_list,
+  true_dlt_ss_other_list
+)
+names(true_dlt_ss_all_list) <- c("tpt", "crm", "other")
+true_dlt_ss_all_list
+true_dlt_ss_all_list$tpt
+true_dlt_ss_all_list$crm
+true_dlt_ss_all_list$other
+
+###1.7.2 Numbers of simulations
+n_sims_all_list <- list(
+  n_sims_tpt,
+  n_sims_crm,
+  n_sims_other
+)
+names(n_sims_all_list) <- c("tpt", "crm", "other")
+n_sims_all_list
+n_sims_all_list$tpt
+n_sims_all_list$crm
+n_sims_all_list$other
 
 
 
 
 
-#2.0 CODING THE MAIN UI: 4 DESIGN TABS (CONFIGURATIONS, DESIGN 1-3, COMPARISON) PLUS 1 CONDUCT TAB
+#2.0 MOCK CONFIGURATIONS TAB
 
-#For each possible recommended design, defining a function containing its design-specific and simulation parameters
-#Depending on the ranking vector, these will be selected and then be called inside fluidPage()
-#This is so adding new designs beyond 3 plus 3 and CRM and new parameters is easier as the project advances
+#For each possible recommended design, defining a function containing its design-specific parameter inputs (as defined above)
+#Defining a function database containing these functions
+#Depending on the ranking vector, functions will be selected from the database and called inside the Configurations tab's columns
+#(Xiaoran suggested adding a "generic function that could be adapted for any future methods, perhaps even a database", so most of this section will be changed)
 
-input_func_3p3 <- function(input) {
+##2.1 3+3 parameter inputs function
+input_func_tpt <- function() {
   message <-
     "3+3-specific parameters"
 
   tpt_allow_deesc_input <- checkboxInput("tpt_allow_deesc_input",
     "Allow any de-escalation for 3+3", value = tpt_allow_deesc)
 
-  true_dlt_ss_3p3_1_input <- textInput("true_dlt_ss_3p3_1_input",
-    "Simulation scenario 1 true DLT rates vector (3+3)", value = true_dlt_ss_3p3_1)
-  true_dlt_ss_3p3_2_input <- textInput("true_dlt_ss_3p3_2_input",
-    "Simulation scenario 2 true DLT rates vector (3+3)", value = true_dlt_ss_3p3_2)
-  true_dlt_ss_3p3_3_input <- textInput("true_dlt_ss_3p3_3_input",
-    "Simulation scenario 3 true DLT rates vector (3+3)", value = true_dlt_ss_3p3_3)
+  true_dlt_ss_tpt_1_input <- textInput("true_dlt_ss_tpt_1_input",
+    "Simulation scenario 1 true DLT rates vector (3+3)", value = true_dlt_ss_tpt_1)
+  true_dlt_ss_tpt_2_input <- textInput("true_dlt_ss_tpt_2_input",
+    "Simulation scenario 2 true DLT rates vector (3+3)", value = true_dlt_ss_tpt_2)
+  true_dlt_ss_tpt_3_input <- textInput("true_dlt_ss_tpt_3_input",
+    "Simulation scenario 3 true DLT rates vector (3+3)", value = true_dlt_ss_tpt_3)
 
-  n_sims_3p3_input <- numericInput("n_sims_3p3_input",
-    "Number of simulations per scenario (3+3)", value = n_sims_3p3)
+  n_sims_tpt_input <- numericInput("n_sims_tpt_input",
+    "Number of simulations per scenario (3+3)", value = n_sims_tpt)
 
   output <- list(message,
     tpt_allow_deesc_input,
-    true_dlt_ss_3p3_1_input, true_dlt_ss_3p3_2_input, true_dlt_ss_3p3_3_input,
-    n_sims_3p3_input)
+    true_dlt_ss_tpt_1_input, true_dlt_ss_tpt_2_input, true_dlt_ss_tpt_3_input,
+    n_sims_tpt_input)
 
   return(output)
 }
 
-input_func_crm <- function(input) {
+##2.2 CRM parameter inputs function
+input_func_crm <- function() {
   message <-
     "CRM-specific parameters"
 
@@ -176,65 +195,108 @@ input_func_crm <- function(input) {
   return(output)
 }
 
-input_func_other <- function(input) {
+#2.3 "Other" design parameter inputs function
+input_func_other <- function() {
   message <-
     "[Design Name Here]-specific parameters"
 
-  mock_parameter_input = sliderInput("mock_parameter_input",
-    "Mock parameter", 0, 2, value = mock_parameter)
+  other_allow_deesc_input <- checkboxInput("other_allow_deesc_input",
+  "Allow any de-escalation for [Design Name Here]", value = other_allow_deesc)
 
-  true_dlt_ss_other_1_input = textInput("true_dlt_ss_other_1_input",
+  true_dlt_ss_other_1_input <- textInput("true_dlt_ss_other_1_input",
     "Simulation scenario 1 true DLT rates vector ([Design Name Here])", value = true_dlt_ss_other_1)
-  true_dlt_ss_other_2_input = textInput("true_dlt_ss_other_2_input",
+  true_dlt_ss_other_2_input <- textInput("true_dlt_ss_other_2_input",
     "Simulation scenario 2 true DLT rates vector ([Design Name Here])", value = true_dlt_ss_other_2)
-  true_dlt_ss_other_3_input = textInput("true_dlt_ss_other_3_input",
+  true_dlt_ss_other_3_input <- textInput("true_dlt_ss_other_3_input",
     "Simulation scenario 3 true DLT rates vector ([Design Name Here])", value = true_dlt_ss_other_3)
 
-  n_sims_other_input = numericInput("n_sims_other_input",
+  n_sims_other_input <- numericInput("n_sims_other_input",
     "Number of simulations per scenario ([Design Name Here])", value = n_sims_other)
 
   output <- list(message,
-    mock_parameter_input,
+    other_allow_deesc_input,
     true_dlt_ss_other_1_input, true_dlt_ss_other_2_input, true_dlt_ss_other_3_input,
     n_sims_other_input)
 
   return(output)
 }
 
-#Selecting which functions to be called within UI based on ranking vector
-func_list <- list(input_func_3p3(), input_func_crm(), input_func_other())
-names(func_list) <- c("3p3", "crm", "other")
-func_1 <- func_list[ranking[1]]
-func_2 <- func_list[ranking[2]]
-func_3 <- func_list[ranking[3]]
+##2.4 "Other2" design parameter inputs function
+input_func_other2 <- function() {
+  return(mock_parameter)
+}
 
-#fluidPage() - backbone of UI
+##2.5 Function database
+func_database <- list(input_func_tpt(), input_func_crm(), input_func_other(), input_func_other2())
+names(func_database) <- c("tpt", "crm", "other", "other2")
+
+##2.6 Selecting functions for Configuration tab columns
+configuration_columns <- list() #Will store column() objects to be called inside fluidPage()
+select_configuration_columns <- function() {
+  for(n in 1:length(ranking)) {
+    configuration_columns[[n]] <- column(n, func_database[ranking[n]], width = 3)
+  }
+  return(configuration_columns)
+}
+
+
+
+
+
+#3.0 MOCK CONDUCT TAB
+
+##3.1 Potential CRM conduct parameter inputs function, based on Christina's prototype CRM app
+input_func_crm_conduct <- function() {
+  next_cohort_input <- sliderInput("next_cohort_input", "Next cohort number (e.g., if two cohorts have been completed, this should be 3)", 2, 10, value = 2)
+  dose_levels_input <- textInput("dose_levels_input", "Dose levels of previous cohorts delimited by comma (e.g., 3, 4)")
+  outcome_input <- textInput("outcome_input", "Number of observed DLT events observed in previous cohorts delimited by comma (e.g., 0, 1)")
+  output = list(next_cohort_input, dose_levels_input, outcome_input)
+  return(output)
+}
+
+##3.2 Potential function to convert the above inputs into something that can be fitted to a get_dfcrm() object (e.g., 3NNN 4NNY)
+##Not finished; hence why it is commented out.
+##convert_crm_conduct_inputs <- function(x = dose_levels_input, y = outcome_input) {
+  ##n_of_previous_cohorts <- length(dose_levels_input)
+  ##inferred_cohort_size <-
+##}
+
+####################################################################################################
+##3.X Mock backend modelling
+##Coding the CRM model object just so I can make a somewhat functional mock Conduct tab
+model_obj_CRM <- get_dfcrm(target = ttl, skeleton = prior_ttp, scale = sqrt(prior_var))
+model_obj_CRM <- stop_at_n(model_obj_CRM, n = max_n)
+model_obj_CRM <- dont_skip_doses(model_obj_CRM, when_escalating = dont_skip_esc, when_deescalating = dont_skip_deesc)
+model_obj_CRM <- stop_when_too_toxic(model_obj_CRM, dose = 1, ttl + stop_tox_x, confidence = stop_tox_y)
+model_obj_CRM <- stop_when_n_at_dose(model_obj_CRM, dose = "recommended", n = stop_n_mtd)
+####################################################################################################
+
+
+
+
+
+#4.0 MAIN UI BACKBONE (CONFIGURATIONS TAB + DESIGN TABS + COMPARISON TAB + CONDUCT TAB)
+
+##4.1 Depending on ranking vector, selecting tabs and their content functions
+ui_tabs <- list()
+select_ui_tabs <- function() {
+
+  ui_tabs[[1]] <- tabPanel("Parameter Configurations", fluidRow(select_configuration_columns()))
+
+  for(i in 2:(length(ranking)+1)) {
+    ui_tabs[[i]] <- tabPanel(title = ranking[i-1], ranking[i-1]) #Functions to add Design tabs' contents should be indexed and called in lieu of this last argument
+  }
+
+  ui_tabs[[length(ranking)+2]] <- tabPanel("Cross-Method Comparison", "Cross-Method Comparison") #Function to add Comparison tab contents should be called in lieu of of this last argument
+  
+  ui_tabs[[length(ranking)+3]] <- tabPanel("Conduct", "Conduct") #Functions to add Conduct tab contents should be indexed and called in lieu of this last argument
+  
+  return(ui_tabs)
+}
+
+##4.2 Running the UI and the app
 main_ui <- fluidPage(
-  tabsetPanel(
-    tabPanel(
-      "Parameter Configurations",
-      fluidRow(
-        column(1, width = 3, func_1),
-        column(2, width = 3, func_2),
-        column(3, width = 3, func_3)
-      )
-    ),
-    tabPanel(
-      ranking[1]
-    ),
-    tabPanel(
-      ranking[2]
-    ),
-    tabPanel(
-      ranking[3]
-    ),
-    tabPanel(
-      "Cross-Method Comparison"
-    ),
-    tabPanel(
-      "Conduct Tab"
-    )
-  )
+  do.call(tabsetPanel, c(select_ui_tabs()))
 )
-main_server <- function(input, output, session) {}
+main_server <- function(input, output) {}
 shinyApp(main_ui, main_server)
