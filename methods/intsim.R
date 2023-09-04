c7ce3a1623c7f5ccca67629d97cc2fa287c18abf
Jake Holton <jakeholton7@gmail.com>
2023-09-04 13:57:06 +0100

    refactored code

--- methods/intsim.R
+++ methods/intsim.R
@@ -1,10 +1,10 @@
+# reads config from a .yaml file
 model_config <- yaml::yaml.load_file("~/dosefinderhub/methods/config.yaml")
-library(escalation)
 
 # setup piping
 `%>%` <- magrittr::`%>%`
 
-# Xiaoran's custom function for loading a function from a package
+# xiaoran's custom function for loading a function from a package
 get_function_from_package <- function(full_func_name) {
   func_name <- unlist(strsplit(full_func_name, "::"))
   package_name <- func_name[1]
@@ -13,7 +13,7 @@ get_function_from_package <- function(full_func_name) {
   return(func)
 }
 
-# Function for model creation
+# function for model creation
 create_model <- function(model_type, model_config) {
   config <- model_config[[model_type]]
   func_name <- get_function_from_package(config$func)
@@ -31,6 +31,8 @@ create_model <- function(model_type, model_config) {
   return(model)
 }
 
+# function for conducting sims and returning output table
+
 process_sims <- function(model, start_dose, n_sims, true_dlt_ss, ...) {
   sims <- model %>%
   escalation::simulate_trials(next_dose = start_dose, n_sims, true_dlt_ss, NULL)
@@ -45,17 +47,20 @@ process_sims <- function(model, start_dose, n_sims, true_dlt_ss, ...) {
   treatedpct <- treated / sum(treated)
   
   # coerce into table
-  
   selection_df <- data.frame(selection)
   treatedpct_df <- data.frame(treatedpct)
   selection_tab <- rbind(t(selection_df), c(NA,t(treatedpct_df)), c(NA,true_dlt_ss))
   
   rownames(selection_tab) <- c("% Dose Selected as MTD", "% Patients Treated at Dose", "True Toxicity Probabilities")
 
+  # return both sims object and table
+
+  pslist <- list(sims, selection_tab)
+  return(pslist)
 }
 
+# example usage of create_model() and process_sims():
 
+# o_model <- create_model("tpt", model_config) 
 
-test <- create_model("tpt", model_config) 
-
-test2 <- test %>% process_sims("tpt", start_dose = 1, n_sims = 100, true_dlt_ss = c(0.01,0.02,0.03,0.2,1))
+# o_sims <- o_model %>% process_sims("tpt", start_dose = 1, n_sims = 100, true_dlt_ss = c(0.01,0.02,0.05,0.2,0.8))
