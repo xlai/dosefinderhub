d0bbef29fbad2ed09fb4d5228ae4dddd20d2eeca
Sian Dennett <dennettsian@gmail.com>
2023-09-06 14:03:19 +0100

    Small changes to adhere to coding guidelines

--- app/modules/questionnaire/ui.R
+++ app/modules/questionnaire/ui.R
@@ -4,23 +4,23 @@ ui <- shiny::fluidPage(
 
   theme = bslib::bs_theme(bootswatch = "darkly"),
 
-  titlePanel("Welcome to the Dose Finder Hub!"),
+  shiny::titlePanel("Welcome to the Dose Finder Hub!"),
 
-  mainPanel(
+  shiny::mainPanel(
 
-    h4("Please answer the questions below to get 
+    shiny::h4("Please answer the questions below to get 
        recommended trial designs tailored to your
        needs, or upload previously saved responses."),
 
     shiny::fileInput("file_upload", "Upload Previous Responses:",
                      accept = c(".csv", ".rds")),
 
-    uiOutput("questionsUI"),
+    shiny::uiOutput("questionsUI"),
 
     shiny::fluidRow(
-      actionButton("get_rating",
-                   label = "Generate recommendations!",
-                   width = 500)
+      shiny::actionButton("get_rating",
+                          label = "Generate recommendations!",
+                          width = 500)
     )
   ),
 
@@ -32,4 +32,4 @@ ui <- shiny::fluidPage(
 
 )
 
-shinyApp(ui, server)
+shiny::shinyApp(ui, server)
