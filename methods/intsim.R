fcddfcfe7a3251713fe563f3d1a758dd0774ae64
Jake Holton <jakeholton7@gmail.com>
2023-09-08 19:55:36 +0100

    fixed sims for .rds dummy data (crm temp broken)

--- methods/intsim.R
+++ methods/intsim.R
@@ -1,60 +1,68 @@
-# reads config from a .yaml file
-#model_config <- yaml::yaml.load_file("~/dosefinderhub/methods/config.yaml")
+# read in .rData file
+design <- "tpt"
+data <- readRDS("dummy_data1.RData")
+attach(data)
+# the data can now be called directly with trial, method, ranking
 
-# setup piping
+# setup piping. we want to avoid loading packages with library()
 `%>%` <- magrittr::`%>%`
 
-# xiaoran's custom function for loading a function from a package
-get_function_from_package <- function(full_func_name) {
-  func_name <- unlist(strsplit(full_func_name, "::"))
-  package_name <- func_name[1]
-  function_name <- func_name[2]
-  func <- getExportedValue(package_name, function_name)
-  return(func)
-}
+# Xiaoran's custom function for loading a function from a package
+#get_function_from_package <- function(full_func_name) {
+  #func_name <- unlist(strsplit(full_func_name, "::"))
+ # package_name <- func_name[1]
+ # function_name <- func_name[2]
+ # func <- getExportedValue(package_name, function_name)
+ # return(func)
+#}
 
 # function that processes config into machine-readable format
 process_config <- function(model_config, model_type){
-  # assume model_config is a list of three elements - trial, method and ranking
+  # model_config is a list of three elements - trial, method and ranking
   # subset config that contains only model_type
-  # select only the q_vaiable and value (currently X (blank) and answer)
-  config_method <- model_config %>% dplyr::filter(method == model_type) %>% dplyr::select(X, answer)
-  config_trial <- model_config %>% dplyr::filter(method == "config") %>% dplyr::select(X, answer)
+  config_method <- model_config$method %>% dplyr::filter(design == model_type) %>% dplyr::select(q_variable, value)
+  # the trial object only contains non-specific info, but we still need to subset the config-specific info
+  config_trial <- model_config$trial %>% dplyr::filter(config == "Y") %>% dplyr::select(q_variable, value)
+
   config_processed <- rbind(config_method, config_trial)
-  # change No to F, Yes to T
-  config_processed[config_processed == "No"] <- FALSE 
-  config_processed[config_processed == "Yes"] <- TRUE
+  # change No to 0, Yes to 1. Not T/F just so that I may convert all to numeric easily!
+  config_processed[config_processed == "No"] <- 0
+  config_processed[config_processed == "Yes"] <- 1
 
-  # convert this dataframe into a list, where each element is q_variable
-  config_processed_list <- as.list(config_processed$answer)
-  names(config_processed_list) <- as.list(config_processed$X)
-
-  # convert to numeric
-  config_processed_list <- sapply(config_processed_list, function(x) {
-    if (is.na(as.numeric(x))){
-      return(x)
-      } else {
-        return(as.numeric(x))
-             }
+  #convert this dataframe into a list, where each element is q_variable
+  config_processed_list <- as.list(as.numeric(config_processed$value)) # Note: this may need to be changed for future non-numeric config values
+  names(config_processed_list) <- as.list(config_processed$q_variable)
 
 return(config_processed_list)
-})
 
 }
-
-test <- process_config(read.csv("methods/dummy_data1.csv"), "tpt")
 
 # function for model creation
 create_model <- function(model_config, model_type) {
   # pre-process config
   config <- process_config(model_config, model_type)
+
   # map the variables from config to the corresponding escalation functions
-  func_name <- get_function_from_package(config$func)
-  model <- do.call(func_name, config$args)
+  # only those directly used as arguments in the model need to be mapped. not needed for e.g. stopping rules
+  # the !! is to stop an error from occuring when the variable to be renamed does not exist
+  
+ # config_names <- names(config)
+
+  #config_names[config_names == "n_doses"] <- "num_doses"
+ # config_names[config_names == "tpt_allow_deesc"] <- "allow_deescalate"
+ # config_names[config_names == "prior_ttp"] <- "skeleton"
+ # config_names[config_names == "ttl"] <- "target"
+ # config_names[config_names == "prior_var"] <- "scale"
+  
+ # names(config) <- config_names
+
+ # oops, this isn't needed as it isn't being fed directly into args anymore since the .yaml code. will delete soon
+
+  #func_name <- get_function_from_package(config)
   model <- switch(model_type,
-  tpt = escalation::get_three_plus_three(model_config) %>%
+  tpt = escalation::get_three_plus_three(config$n_doses, config$tpt_allow_deesc) %>%
     escalation::stop_at_n(n = config$max_n),
-  crm = escalation::get_dfcrm(model_config) %>% 
+  crm = escalation::get_dfcrm(config$n_doses, config$prior_ttp, config$ttl, config$prior_var) %>% 
     escalation::dont_skip_doses(when_escalating = 1-config$skip_esc, when_deescalating = 1-config$skip_deesc) %>% 
     escalation::stop_when_too_toxic(dose = 1, config$stop_tox_x + config$target, confidence = config$stop_tox_y) %>%
     escalation::stop_when_n_at_dose(n = config$stop_n_mtd, dose = "recommended") %>%
@@ -64,19 +72,20 @@ create_model <- function(model_config, model_type) {
   return(model)
 }
 
-# function for conducting sims and returning output table
+# function for conducting sims and returning output
 
-process_sims <- function(model, model_type, model_config, sim_config) {
-  
-  config <- process_config(model_config)
-  # sim_config, a lists consists scenarios, n_sims
+process_sims <- function(model_config, model_type, sim_config) {
 
-  # process scenarios into a list (check)
-  sim_scenarios <- as.list(sim_config$scenarios)
+  config <- process_config(model_config, model_type)
+  model <- create_model(model_config, model_type)
+  # IMPORTANT: sim_config <- process_config(sim_config)
+  # sim_config is a list that consists scenarios, n_sims and display options. to be expanded upon with UI development
+
+  # sim_scenarios <- as.list(sim_config$scenarios)
   # assume n_sims is a list (name of each element would be model_type)
 
   sims <- model %>%
-  escalation::simulate_trials(next_dose = config$start_dose, n_sims$model_type, config$true_dlt_ss, NULL)
+  escalation::simulate_trials(next_dose = config$start_dose, num_sims = 100, true_prob_tox = c(0.05,0.1,0.3,0.4)) # note: temp direct use of n_sims, true DLTs. this SHOULD be part of sim_config
   
   # find selection probs
   selection <- sims %>% 
@@ -90,7 +99,7 @@ process_sims <- function(model, model_type, model_config, sim_config) {
   # coerce into table
   selection_df <- data.frame(selection)
   treatedpct_df <- data.frame(treatedpct)
-  selection_tab <- rbind(t(selection_df), c(NA,t(treatedpct_df)), c(NA,true_dlt_ss))
+  selection_tab <- rbind(t(selection_df), c(NA,t(treatedpct_df)), c(NA,c(0.05,0.1,0.3,0.4)))
   
   rownames(selection_tab) <- c("% Dose Selected as MTD", "% Patients Treated at Dose", "True Toxicity Probabilities") 
 
@@ -100,12 +109,7 @@ process_sims <- function(model, model_type, model_config, sim_config) {
   return(pslist)
 }
 
-# example usage of create_model() and process_sims():
-
-o_model <- create_model("crm", model_config) 
-
-o_sims <- o_model %>% process_sims(start_dose = 1, n_sims = 10, true_dlt_ss = c(0.01,0.05,0.2,0.8))
+o_sims <- process_sims(data, design)
 
 # add error for number of doses in true_dlt_ss being different to num_doses!
 # or programmatically make it impossible? reactibve table input?
-
--- methods/testarea.R
+++ methods/testarea.R
@@ -1,35 +1,72 @@
-# reads config from a .yaml file
-#model_config <- yaml::yaml.load_file("~/dosefinderhub/methods/config.yaml")
+# read in .rData file
+data <- readRDS("dummy_data1.RData")
+attach(data)
+# the data can now be called directly with trial, method, ranking
 
-# setup piping
+# setup piping. we want to avoid loading packages with library()
 `%>%` <- magrittr::`%>%`
 
-# xiaoran's custom function for loading a function from a package
-get_function_from_package <- function(full_func_name) {
-  func_name <- unlist(strsplit(full_func_name, "::"))
-  package_name <- func_name[1]
-  function_name <- func_name[2]
-  func <- getExportedValue(package_name, function_name)
-  return(func)
-}
+# Xiaoran's custom function for loading a function from a package
+#get_function_from_package <- function(full_func_name) {
+  #func_name <- unlist(strsplit(full_func_name, "::"))
+ # package_name <- func_name[1]
+ # function_name <- func_name[2]
+ # func <- getExportedValue(package_name, function_name)
+ # return(func)
+#}
 
 # function that processes config into machine-readable format
 process_config <- function(model_config, model_type){
-  # assume model_config is a list of three elements - trial, method and ranking
+  # model_config is a list of three elements - trial, method and ranking
   # subset config that contains only model_type
-  # select only the q_vaiable and value (currently X (blank) and answer)
-  config_method <- model_config %>% dplyr::filter(method == model_type) %>% dplyr::select(X, answer)
-  config_trial <- model_config %>% dplyr::filter(method == "config") %>% dplyr::select(X, answer)
-  config_processed <- rbind(config_method, config_trial)
-  # change No to F, Yes to T
-  config_processed[config_processed == "No"] <- FALSE 
-  config_processed[config_processed == "Yes"] <- TRUE
+  config_method <- model_config$method %>% dplyr::filter(design == model_type) %>% dplyr::select(q_variable, value)
+  # the trial object only contains non-specific info, but we still need to subset the config-specific info
+  config_trial <- model_config$trial %>% dplyr::filter(config == "Y") %>% dplyr::select(q_variable, value)
 
-  # convert this dataframe into a list, where each element is q_variable
-  config_processed_list <- as.list(config_processed$answer)
-  names(config_processed_list) <- as.list(config_processed$X)
+  config_processed <- rbind(config_method, config_trial)
+  # change No to 0, Yes to 1. Not T/F just so that I may convert all to numeric easily!
+  config_processed[config_processed == "No"] <- 0
+  config_processed[config_processed == "Yes"] <- 1
+
+  #convert this dataframe into a list, where each element is q_variable
+  config_processed_list <- as.list(as.numeric(config_processed$value)) # Note: this may need to be changed for future non-numeric config values
+  names(config_processed_list) <- as.list(config_processed$q_variable)
 
 return(config_processed_list)
+
 }
 
-test <- process_config(read.csv("methods/dummy_data1.csv"), "tpt")
+
+# function for model creation
+create_model <- function(model_config, model_type) {
+  # pre-process config
+  config <- process_config(model_config, model_type)
+
+  # map the variables from config to the corresponding escalation functions
+  # only those directly used as arguments in the model need to be mapped. not needed for e.g. stopping rules
+  # the !! is to stop an error from occuring when the variable to be renamed does not exist
+  
+ # config_names <- names(config)
+
+  #config_names[config_names == "n_doses"] <- "num_doses"
+ # config_names[config_names == "tpt_allow_deesc"] <- "allow_deescalate"
+ # config_names[config_names == "prior_ttp"] <- "skeleton"
+ # config_names[config_names == "ttl"] <- "target"
+ # config_names[config_names == "prior_var"] <- "scale"
+  
+ # names(config) <- config_names
+
+  #func_name <- get_function_from_package(config)
+  model <- switch(model_type,
+  tpt = escalation::get_three_plus_three(config$n_doses, config$tpt_allow_deesc) %>%
+    escalation::stop_at_n(n = config$max_n),
+  crm = escalation::get_dfcrm(config$n_doses, config$prior_ttp, config$ttl, config$prior_var) %>% 
+    escalation::dont_skip_doses(when_escalating = 1-config$skip_esc, when_deescalating = 1-config$skip_deesc) %>% 
+    escalation::stop_when_too_toxic(dose = 1, config$stop_tox_x + config$target, confidence = config$stop_tox_y) %>%
+    escalation::stop_when_n_at_dose(n = config$stop_n_mtd, dose = "recommended") %>%
+    escalation::stop_at_n(n = config$max_n)
+  )
+
+  return(model)
+}
+out <- create_model(data,"tpt")
