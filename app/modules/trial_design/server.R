1eabbbdd45a72e7b0ec1d316b613ab197c1fff1a
Xiaoran Lai <xiaoran.lai@icr.ac.uk>
2023-09-14 13:38:10 +0100

    Refactor UI code: separate UI and server functions.
    - Split UI and server logic into distinct sections for clearer organization.
    - Enhanced modularity for easier maintenance and potential scaling.

--- app/modules/trial_design/server.R
+++ app/modules/trial_design/server.R
@@ -1,1 +1,140 @@
-main_server = function(input, output, session) {}
+
+column_names <- sprintf("d(%d)", 1:n_doses)
+#reactive_cohort_size <- dummy_data_trial[which(dummy_data_trial$q_variable=="cohort_size"), "value"]
+
+server_all <- function(input, output, session) {
+  
+
+  ######################################## Configuration tab's simulation scenarios table code ########################################
+
+  #Initialize empty data frame with specified columns
+  reactive_table_data <- reactiveVal(data.frame(matrix(ncol = n_doses, nrow = 0, dimnames = list(NULL, column_names))))
+  
+  observe({
+    #Capture current data
+    current_data <- reactive_table_data()
+    
+    #Calculate rows to add or remove
+    target_rows <- as.numeric(input$n_scenarios_input)
+    current_rows <- nrow(current_data)
+    rows_to_add <- target_rows - current_rows
+    
+    #Update data based on the difference
+    if (rows_to_add > 0) {
+      new_rows <- data.frame(
+        Scenario = seq_len(rows_to_add) + current_rows,
+        matrix(0, ncol = n_doses, nrow = rows_to_add, dimnames = list(NULL, column_names))
+        )
+      updated_data <- rbind(current_data, new_rows)
+    } else {
+      updated_data <- head(current_data, target_rows)
+    }
+    
+    #Update reactive data frame
+    reactive_table_data(updated_data)
+  })
+  
+  output$editable_table <- renderDT({
+    datatable(reactive_table_data(), editable = TRUE, options = list(columnDefs = list(list(className = 'dt-center', targets = "_all"), list(targets = 0, className = "not-editable"))),
+              rownames = FALSE)
+  }, server = FALSE)
+  
+  output$table_output <- renderDT({
+    datatable(reactive_table_data(), editable = TRUE, options = list(columnDefs = list(list(className = 'dt-center', targets = "_all"))), rownames = F)
+  })
+  
+  observeEvent(input$table_output_cell_edit, {
+    info <- input$table_output_cell_edit
+    modified_data <- reactive_table_data()
+    modified_data[info$row, (info$col + 1)] <- as.numeric(info$value)
+    reactive_table_data(modified_data)
+  })
+  
+  observeEvent(input$plot_button, {
+    #Capture current data and transform for plotting
+    current_data <- reactive_table_data()
+    plot_data <- tidyr::gather(current_data[, -1, drop = FALSE], key = "Dose Level", value = "Value")
+    plot_data$Scenario <- rep(current_data$Scenario, each = ncol(current_data) - 1)
+    
+    #Create the plot
+    p <- ggplot(plot_data, aes(x = `Dose Level`, y = Value)) +
+      geom_line() +
+      geom_point() +
+      labs(x = "Dose Levels",
+           y = "'True' DLT Rates") +
+      theme_minimal()
+    
+    # Render the plot
+    output$plot <- renderPlot({
+      p
+    })
+  })
+  
+
+  ######################################## Simulation tab server code ########################################
+
+  new_n_scen <- reactive(as.numeric(input$n_scenarios_input))
+  updated_scen_choices <- reactive(paste0("Scenario ", 1:new_n_scen()))
+  
+  output$scen_output_question <- renderUI({
+    tagList(
+      selectizeInput("scen_output_input", "Select scenarios", choices = updated_scen_choices(),
+        multiple = TRUE, list(plugins = list('remove_button')))
+    )
+  })
+
+
+  ######################################## Conduct tab server code ########################################
+
+  reactive_cohort_size <- reactive(as.numeric(input$cohort_size))
+
+  output$treated_participants_question <- renderUI({
+    tagList(
+      numericInput("treated_participants", "How many participants have been treated and observed for DLT?", value = reactive_cohort_size())
+    )
+  })
+
+
+  ######################################## Conduct tab table code ########################################
+
+conduct_reactive_table_data <- reactiveVal(data.frame(matrix(ncol = 3, nrow = 0, dimnames = list(NULL, c("Cohort number", "Dose level", "DLT?")))))
+
+  observe({
+    conduct_current_data <- conduct_reactive_table_data()
+    conduct_current_rows <- nrow(conduct_current_data)
+    reactive_treated_participants <- as.numeric(input$treated_participants)
+    conduct_target_rows <- reactive_treated_participants
+    conduct_rows_to_add <- conduct_target_rows-conduct_current_rows
+
+    if (conduct_rows_to_add > 0) {
+      conduct_new_rows <- data.frame(
+        Participants = seq_len(conduct_rows_to_add) + conduct_current_rows,
+        matrix(0, ncol = 3, nrow = conduct_rows_to_add, dimnames = list(NULL, c("Cohort number", "Dose level", "DLT?")))
+      )
+      conduct_updated_data <- rbind(conduct_current_data, conduct_new_rows)
+    } else {
+      conduct_updated_data <- head(conduct_current_data, conduct_target_rows)
+    }
+
+    conduct_reactive_table_data(conduct_updated_data)
+  })
+
+  output$conduct_editable_table <- renderDT({
+    datable(conduct_reactive_table_data(), editable = TRUE,
+    options = list(columnDefs = list(list(className = 'dt-center', targets = "_all"),
+      list(targets = 0, className = "not-editable")
+    )))
+  }, server = FALSE)
+
+  output$conduct_table_output <- renderDT({
+    datatable(conduct_reactive_table_data(), editable = TRUE, options = list(columnDefs = list(list(className = 'dt-center', targets = "_all"))), rownames = F)
+  })
+
+  observeEvent(input$conduct_table_output_cell_edit, {
+    conduct_info <- input$conduct_table_output_cell_edit
+    conduct_modified_data <- conduct_reactive_table_data()
+    conduct_modified_data[conduct_info$row, (conduct_info$col + 1)] <- as.numeric(conduct_info$value)
+    conduct_reactive_table_data(conduct_modified_data)
+  })
+
+}
