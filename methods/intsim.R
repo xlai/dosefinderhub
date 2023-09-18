ebc4794f70b2551c5a1e37d1072d530b8703956c
Xiaoran Lai <xiaoran.lai@icr.ac.uk>
2023-09-18 13:30:04 +0100

    Removed redundancy and corrected create_model - Removed redundancy from previous merging - Corrected dependency on the use of global variables from attaching model_config in create_model()

--- methods/intsim.R
+++ methods/intsim.R
@@ -1,28 +1,12 @@
 # read in .rData file
-design <- "crm"
+
 data <- readRDS("dummy_data.RData")
-attach(data)
+#attach(data)
 # the data can now be called directly with trial, method, ranking subsets
 
 # setup piping. we want to avoid loading packages with library()
 `%>%` <- magrittr::`%>%`
 
-# create some sim data
-create_dummy_sims <- function(n_doses) {
-
-  n_sims <- sample(seq(20, 100, 10), 1)
-
-  true_dlt_ss <- list()
-  true_dlt_ss[[1]] <- sort(sample(seq(0, 1, 0.01), n_doses))
-  true_dlt_ss[[2]] <- sort(sample(seq(0, 1, 0.01), n_doses))
-  true_dlt_ss[[3]] <- sort(sample(seq(0, 1, 0.01), n_doses))
-
-  value <- list(n_sims = n_sims, true_dlt_ss = true_dlt_ss)
-
-  saveRDS(value, "dummy_sims_data")
-  dummy_sims_data <- readRDS("dummy_sims_data")
-  return(dummy_sims_data)
-}
 # create some sim data
 create_dummy_sims <- function(n_doses) {
 
@@ -59,19 +43,6 @@ process_config <- function(model_config, model_type){
     as.list(config_processed$value), 
     as.list(config_processed$q_variable)
   )
-  # format all data
-  for (i in 1:length(config_processed_list)){
-      if (config_processed$answer_type[i] == "numeric"){
-        config_processed_list[[i]] <- as.numeric(config_processed$value[i])}
-      else if (config_processed$answer_type[i] == "numeric_bounded"){
-        config_processed_list[[i]] <- as.numeric(config_processed$value[i])}
-      else if (config_processed$value[i] == "No"){
-        config_processed_list[[i]] <- FALSE}
-      else if (config_processed$value[i] == "Yes"){
-        config_processed_list[[i]] <- TRUE}
-      else if (config_processed$answer_type[i] == "comma-separated list"){
-        config_processed_list[[i]] <- as.numeric(unlist(strsplit(config_processed_list[[i]],",")))}
-  }
 
   # format all data
   for (i in 1:length(config_processed_list)){
@@ -105,10 +76,12 @@ create_model <- function(model_config, model_type) {
     escalation::stop_when_too_toxic(dose = 1, config$stop_tox_x + config$target, confidence = config$stop_tox_y) %>%
     escalation::stop_when_n_at_dose(n = config$stop_n_mtd, dose = "recommended") %>%
     escalation::stop_at_n(n = config$max_n),
-  boin = escalation::get_boin(num_doses = n_doses, target = ttl, use_stopping_rule = use_stopping_rule, p.saf = p_saf,
-                              p.tox = p_tox, cutoff.eli = 0.95, extrasafe = FALSE, offset = 0.05) %>%
-    escalation::stop_when_n_at_dose(n=14, dose = "recommended") %>%
-    escalation::stop_at_n(n = max_n)
+  boin = escalation::get_boin(
+    num_doses = config$n_doses, target = config$ttl, 
+    use_stopping_rule = config$use_stopping_rule, p.saf = config$p_saf,
+    p.tox = config$p_tox, cutoff.eli = 0.95, extrasafe = FALSE, offset = 0.05) %>%
+    escalation::stop_when_n_at_dose(n=config$stop_n_mtd, dose = "recommended") %>%
+    escalation::stop_at_n(n = config$max_n)
   )
 
   return(model)
