d99ebcc48fe1b8535d6d37fa551c81262bc31491
MaddieT <maddie.edwards.t@gmail.com>
2025-07-21 14:31:06 +0100

    changed to nav_panel to get main panel to show up

--- app_skeleton/pages/question_ui.R
+++ app_skeleton/pages/question_ui.R
@@ -4,10 +4,10 @@ library(shiny.semantic)
 question_ui <- function(id) {
   ns <- NS(id)
   page_sidebar(
-   main = div(
-      h3("Trial Design Questionnaire"),
-      p("This section will guide you through a series of questions to identify your needs and preferences for trial design."),
-      p("Click 'Submit' when you are ready to proceed.")
+    nav_panel(
+      h3("Questionnaire"),
+      p("Please answer the following questions to help us determine the best trial design for you."),
+      p("Click 'Submit' to proceed with your answers.")
     ),
     sidebar = sidebar(
       h4("Questionnaire"),
