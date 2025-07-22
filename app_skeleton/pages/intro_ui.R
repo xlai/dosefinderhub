f8842c6caf788379aefcebf74c0d875dcfe13b81
MaddieT <maddie.edwards.t@gmail.com>
2025-07-22 15:50:28 +0100

    second attempt at adding contents page to intro sidebar (got drop down now need to had hyperlinks)

--- app_skeleton/pages/intro_ui.R
+++ app_skeleton/pages/intro_ui.R
@@ -10,29 +10,18 @@ intro_ui <- function(id) {
       ),
       sidebar = sidebar(
         h4("Welcome"),
-        navset_card_tab(
+        accordion_filters <- accordion(
         title = "Table of Contents",
         id = "nav_id",
-        nav_panel(
-            "Section 1",
-            value = "section1",
-            h3("Introduction to Dose Finder Hub"),
-            p("Section 1 info")
+        accordion_panel(
+            "Section 1", icon = icon("menu-app"),
         ),
-        nav_panel(
-            "Section 2",
-            value = "section2",
-            h3("How to Use the Hub"),
-            p("Section 2 info")
-        ),
-        nav_panel(
-            "Section 3",
-            value = "section3",
-            h3("Understanding Trial Design"),
-            p("Section 3 info")
-        ))
+        accordion_panel(
+            "Section 2", icon = icon("menu-app"),
+        )
+        
     )
-      
+   )
   )
 }
 
