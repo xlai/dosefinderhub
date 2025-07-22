ba7ea8cb065e69660f9e39905d5a62245ae83af6
MaddieT <maddie.edwards.t@gmail.com>
2025-07-22 16:18:06 +0100

    Added functional contents page with hyperlinks to intro tab (now need to work on contents of each section)

--- app_skeleton/pages/intro_ui.R
+++ app_skeleton/pages/intro_ui.R
@@ -7,16 +7,42 @@ intro_ui <- function(id) {
     tabPanel(
         h3("Intro"),
         p("This hub provides a user-friendly interface to guide you through the process of determining the appropriate trial design based on your inputs."),
+      tags$head(
+        tags$style(HTML("
+        html {
+        scroll-behaviour: smooth;
+        }
+        h2 {
+        margin-top; 100px;     /* for spacing when jumped to  */
+        }
+        "))
       ),
+
+      tags$div(id = "section1", h2("Why the app was created"), p("Content of section 1......")),
+      tags$div(id = "section2", h2("Background to Trial Designs and Statistics"), p("Content of section 2.....")),
+      tags$div(id = "section3", h3("How DoseFinderHub can help you"), p("Content of section 3....."))
+     ),
+
       sidebar = sidebar(
         h4("Table of Contents"),
         accordion_filters <- accordion(
         id = "nav_id",
         accordion_panel(
-            "Section 1", icon = icon("menu-app"),
+            "Introduction",
+            tags$ul(
+                tags$li(a(href = "#section1", "Why the app was created")),
+                tags$li(a(href = "#section2", "Background to Trial Designs and Statistics")),
+                tags$li(a(href = "#section3", "How DoseFinderHub can help you"))
+            )
         ),
         accordion_panel(
-            "Section 2", icon = icon("menu-app"),
+            "WorkFlow", icon = icon("menu-app"),
+        ),
+        accordion_panel(
+            "Team/Contact", icon = icon("menu-app"),
+        ),
+        accordion_panel(
+            "Guide to Importing", icon = icon("menu-app"),
         )
         
     )
