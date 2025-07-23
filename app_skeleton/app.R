6a5f1125d59198d1db559e3ecae700f5f9acdf70
MaddieT <maddie.edwards.t@gmail.com>
2025-07-23 10:44:52 +0100

    Refactor UI tab definitions and comment out unused source files

--- app_skeleton/app.R
+++ app_skeleton/app.R
@@ -4,30 +4,27 @@ library(shiny)
 library(bslib)
 library(htmltools)
 
-source("app_skeleton/pages/intro_ui.R")
-source("app_skeleton/pages/question_ui.R")
-source("app_skeleton/pages/trial_design_ui.R")
-source("app_skeleton/pages/sim_ui.R")
-source("app_skeleton/pages/con_ui.R")
+
+#source("app_skeleton/pages/intro_ui.R")
+#source("app_skeleton/pages/question_ui.R")
+#source("app_skeleton/pages/trial_design_ui.R")
+#source("app_skeleton/pages/sim_ui.R")
+#source("app_skeleton/pages/con_ui.R")
+#works without sourcing yipeee
 
 # Define UI for the application
 ui <- navbarPage(
     title = "Dose Finder Hub",
     id = "nav",
     theme = bs_theme(version = 5, bootswatch = "flatly"),
-    
     # Intro tab
     tabPanel("Introduction", intro_ui("intro")),
-    
     # Questionnaire tab
     tabPanel("Questionnaire", question_ui("questionnaire")),
-    
     # Results tab
     tabPanel("Trial Design", trial_design_ui("trial_design")),
-    
     # Simulation tab
     tabPanel("Simulation", sim_ui("simulation")),
-    
     # Conduct tab
     tabPanel("Conduct", con_ui("conduct"))
 )
