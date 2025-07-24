040bc99179e21d1769f38044050ac2a78a04e84f
Heather Christie <heather.christie@icr.ac.uk>
2025-07-24 11:51:41 +0100

    Debugging the code and modularising varaibles to allow for Trial Designs tab to run correctly.

--- app_skeleton/pages/trial_design_ui.R
+++ app_skeleton/pages/trial_design_ui.R
@@ -1,14 +1,18 @@
 library(shiny)
 library(shiny.semantic)
 
+trial_design_ui <- function(id) {
+  ns <- NS(id)
+
 #################################### From Configurations Tab UI ####################################
 
+
 # Non-specific UI inputs for trial design
-  n_doses_output <- numericInput("n_doses_inputt", "How many dose levels are being tested?", min = 1, value = "5", step = 1)
-  ttl_output <- numericInput("ttl_inputt", "What is the target toxicity level for this trial, as a decimal?", min = 0, max = 1, value = "0.3", step = 0.01)
-  max_size_output <- numericInput("max_size_inputt", "What is the maximum sample size for this trial?", min = 1, value = "30", step = 1)
-  start_dose_output <- numericInput("start_dose_inputt", "What is the starting dose level?", min = 1, value = "1", step = 1)
-  cohort_output <- numericInput("cohort_inputt", "What size will the cohorts be?", min = 1, value = "5", step = 1)
+  n_doses_output <- numericInput(ns("n_doses_inputt"), "How many dose levels are being tested?", min = 1, value = "5", step = 1)
+  ttl_output <- numericInput(ns("ttl_inputt"), "What is the target toxicity level for this trial, as a decimal?", min = 0, max = 1, value = "0.3", step = 0.01)
+  max_size_output <- numericInput(ns("max_size_inputt"), "What is the maximum sample size for this trial?", min = 1, value = "30", step = 1)
+  start_dose_output <- numericInput(ns("start_dose_inputt"), "What is the starting dose level?", min = 1, value = "1", step = 1)
+  cohort_output <- numericInput(ns("cohort_inputt"), "What size will the cohorts be?", min = 1, value = "5", step = 1)
   non_specific_ui_inputts <- tagList(
     n_doses_output,
     ttl_output,
@@ -19,19 +23,19 @@ library(shiny.semantic)
 
 # Specific UI inputs for trial design
 # CRM specific inputs
-skip_esc_crm_input <- radioButtons("skip_esc_crm_input","Would you like to be able to skip doses when escalating?",
+skip_esc_crm_input <- radioButtons(ns("skip_esc_crm_input"),"Would you like to be able to skip doses when escalating?",
 choices = c("Yes" = TRUE, "No" = FALSE), selected = FALSE, inline = TRUE)
-skip_deesc_crm_input <- radioButtons("skip_deesc_crm_input","Would you like to be able to skip doses when de-escalating?",
+skip_deesc_crm_input <- radioButtons(ns("skip_deesc_crm_input"),"Would you like to be able to skip doses when de-escalating?",
 choices = c("Yes" = TRUE, "No" = FALSE), selected = TRUE, inline = TRUE)
-above_target_input <- radioButtons("above_target_input",
+above_target_input <- radioButtons(ns("above_target_input"),
 "Do you want to prevent escalation of doses if the overall observed DLT rate at the current dose level is above the target DLT rate?",
 choices = c("Yes" = TRUE, "No" = FALSE), selected = TRUE, inline = TRUE)
-prior_var_input <- numericInput("prior_var_input", "What is the estimate of the prior variance?", min = 0, value = 0.1)
-stop_n_mtd_input <- numericInput("stop_n_mtd_input", "What is the minimum number of patients required at recommended dose before early stopping?", min = 1, value = 24)
-skeleton_input <- textInput("skeleton_input", "What are the prior estimates of the DLT rates at each dose? Please make this an increasing list and separate each value with a comma.", value = "0.108321683015674,0.255548628279939,0.425089891767129,0.576775912195444,0.817103320499882")
-prior_mtd_input <- numericInput("prior_mtd_input", "What is your prior guess of the MTD?", min = 1, value = 8)
-stop_tox_x_input <- numericInput("stop_tox_x_input", "When using the this Bayesian safety early criterion: p(true DLT rate at lowest dose > target DLT rate + x | data) > y, what would you like x to be? This is the excess toxicity above the target DLT.", min = 0, value = 0.09)
-stop_tox_y_input <- numericInput("stop_tox_y_input", "What would you like y to be? This is the confidence level for safety stopping.", min = 0, max = 1, value = 0.77)
+prior_var_input <- numericInput(ns("prior_var_input"), "What is the estimate of the prior variance?", min = 0, value = 0.1)
+stop_n_mtd_input <- numericInput(ns("stop_n_mtd_input"), "What is the minimum number of patients required at recommended dose before early stopping?", min = 1, value = 24)
+skeleton_input <- textInput(ns("skeleton_input"), "What are the prior estimates of the DLT rates at each dose? Please make this an increasing list and separate each value with a comma.", value = "0.108321683015674,0.255548628279939,0.425089891767129,0.576775912195444,0.817103320499882")
+prior_mtd_input <- numericInput(ns("prior_mtd_input"), "What is your prior guess of the MTD?", min = 1, value = 8)
+stop_tox_x_input <- numericInput(ns("stop_tox_x_input"), "When using the this Bayesian safety early criterion: p(true DLT rate at lowest dose > target DLT rate + x | data) > y, what would you like x to be? This is the excess toxicity above the target DLT.", min = 0, value = 0.09)
+stop_tox_y_input <- numericInput(ns("stop_tox_y_input"), "What would you like y to be? This is the confidence level for safety stopping.", min = 0, max = 1, value = 0.77)
 specific_ui_inputs_crm <- tagList(
   skip_esc_crm_input,
   skip_deesc_crm_input,
@@ -45,25 +49,26 @@ specific_ui_inputs_crm <- tagList(
 )
 
 # 3+3 specific inputs
-skip_tpt_input <- radioButtons("skip_tpt_input","Would you like to be able to skip doses when de-escalating?",
+skip_tpt_input <- radioButtons(ns("skip_tpt_input"),"Would you like to be able to skip doses when de-escalating?",
 choices = c("Yes" = TRUE, "No" = FALSE), selected = TRUE, inline = TRUE)
 
 # Other specific inputs (placeholder)
-specific_ui_inputs_other <- radioButtons("specific_ui_inputs_other","This is a placeholder. Clicking this button will do nothing.",
+specific_ui_inputs_other <- radioButtons(ns("specific_ui_inputs_other"),"This is a placeholder. Clicking this button will do nothing.",
 choices = c("Yes" = TRUE, "No" = FALSE), selected = TRUE, inline = TRUE)
 
 ########################################### Running the UI ###########################################
 
-trial_design_ui <- function(id) {
-  ns <- NS(id)
+
   page_sidebar( 
       layout_column_wrap( 
    
    # General Trial Design Parameters
      card(full_screen = TRUE,
       card_header("General Trial Parameters"),
+      card_body(
        checkboxInput("display_input_all", "Display parameters", value = F),
        conditionalPanel(condition = "input.display_input_all==1", non_specific_ui_inputts)
+     )
       ),
 
       #Specific Trial Design Parameters
@@ -89,8 +94,7 @@ trial_design_ui <- function(id) {
            conditionalPanel(condition = "input.display_other==1", specific_ui_inputs_other)
           )
         )
-      )
-    ),
+      ),
     sidebar = sidebar(
       h3("Trial Design"),
       
@@ -102,7 +106,9 @@ trial_design_ui <- function(id) {
       downloadButton("config_save_button", "Download file with all configurations"),
       tags$hr(), # Separator line
       p("Done filling out the configurations? Click the button below to run simulations."),
-      actionButton(ns("view_simulation"), "View Simulation")
+      actionButton(ns("view_simulation"), "View Simulation"),
+
+    )
     )
 }
 
@@ -182,7 +188,6 @@ trial_design_server <- function(id) {
   # 3+3
   skip_tpt <- reactive({as.logical(input$skip_tpt_input)}) 
 
-  
-}
-)
-}
+} # End of function within moduleServer
+) # End of moduleServer
+} # End of trial_design_server function
