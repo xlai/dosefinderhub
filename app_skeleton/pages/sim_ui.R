8b0cdcca665d2e505dde79d5fb19b5c25724b044
Heather Christie <heather.christie@icr.ac.uk>
2025-07-24 11:53:55 +0100

    Modularising the variables in the Simulation tab

--- app_skeleton/pages/sim_ui.R
+++ app_skeleton/pages/sim_ui.R
@@ -1,23 +1,25 @@
 library(shiny)
 library(shiny.semantic)
 
-# Simulation-Specific Inputs
-  n_sims_input <- numericInput("n_sims_input", "How many simulations would you like to run per design per scenario?", value = 10)
-  n_scenarios_input <- numericInput("n_scenarios_input", "How many scenarios would you like to simulate?", min = 1, max = 3, value = 3) # Capping the number of scenarios at 3 (for now)
-  text2 <- "Please fill out each scenario's and each dose's 'True' Dose Limiting Toxicity probabilities in the table below:"
-  table_output <- DT::DTOutput("table_output") # This is to test the table output used for the simulations tab.
-  test_df_table <- DT::DTOutput("test_df")
-simulation_inputs <- tagList(
-  n_sims_input,
-  n_scenarios_input,
-  text2,
-  table_output,
-  test_df_table
-)
-
-
 sim_ui <- function(id) {
   ns <- NS(id)
+
+  # Simulation-Specific Inputs
+    n_sims_input <- numericInput(ns("n_sims_input"), "How many simulations would you like to run per design per scenario?", value = 10)
+    n_scenarios_input <- numericInput(ns("n_scenarios_input"), "How many scenarios would you like to simulate?", min = 1, max = 3, value = 3) # Capping the number of scenarios at 3 (for now)
+    text2 <- "Please fill out each scenario's and each dose's 'True' Dose Limiting Toxicity probabilities in the table below:"
+    table_output <- DT::DTOutput(ns("table_output")) # This is to test the table output used for the simulations tab.
+    test_df_table <- DT::DTOutput(ns("test_df"))
+  simulation_inputs <- tagList(
+    n_sims_input,
+    n_scenarios_input,
+    text2,
+    table_output,
+    test_df_table
+  )
+
+
+  # Running the tab itself
   page_sidebar(
     main = div(
       h3("Trial Design Simulation"),
