c8db590ca77d069da113b947ba8d6479189c36e8
MaddieT <maddie.edwards.t@gmail.com>
2025-08-06 16:16:33 +0100

    reactive DLT values are now a tick box

--- app_skeleton/pages/con_ui.R
+++ app_skeleton/pages/con_ui.R
@@ -1,4 +1,3 @@
-# Load required libraries
 library(shiny)
 library(bslib)
 library(DT)
@@ -23,7 +22,13 @@ con_ui <- function(id) {
           tagList(
             plotly_widget,
             numericInput(ns("treated_participants_input"), "Number of Treated Participants", value = 3, min = 0),
-            DTOutput(ns("editable_table"))
+            DTOutput(ns("editable_table")),
+            tags$style(HTML("
+              input[type='checkbox'] {
+                transform: scale(1.2);
+                margin: 5px;
+              }
+            "))
           )
         )
       )
@@ -48,21 +53,35 @@ con_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # Reactive table data
+    # Reactive table data with logical DLT
     conduct_reactive_table_data <- reactiveVal(
-      data.frame(matrix(ncol = 4, nrow = 0, dimnames = list(NULL, c("No.number", "Dose_level", "DLT", "Posterior_DLT"))))
+      data.frame(
+        No.Cohort = integer(0),
+        Dose_Level = numeric(0),
+        DLT = logical(0),  # Logical for checkbox
+        Posterior_DLT = numeric(0),
+        stringsAsFactors = FALSE
+      )
     )
 
-    # Update table based on numeric input
+    # Update table based on numeric input, safely handling NULL or empty
     observe({
       target_rows <- input$treated_participants_input
+
+      # Skip update if input is NULL, NA, or not a valid number
+      if (is.null(target_rows) || is.na(target_rows) || !is.numeric(target_rows)) return()
+
       current_data <- conduct_reactive_table_data()
       current_rows <- nrow(current_data)
       rows_to_add <- target_rows - current_rows
 
       if (rows_to_add > 0) {
         new_rows <- data.frame(
-          matrix(0, ncol = 4, nrow = rows_to_add, dimnames = list(NULL, c("No.Cohort", "Dose_Level", "DLT", "Posterior_DLT")))
+          No.Cohort = integer(rows_to_add),
+          Dose_Level = numeric(rows_to_add),
+          DLT = logical(rows_to_add),
+          Posterior_DLT = numeric(rows_to_add),
+          stringsAsFactors = FALSE
         )
         updated_data <- rbind(current_data, new_rows)
       } else {
@@ -72,14 +91,26 @@ con_server <- function(id) {
       conduct_reactive_table_data(updated_data)
     })
 
-    # Render editable DT table
+    # Render editable DT table with checkbox for DLT
     output$editable_table <- renderDT({
       datatable(
         conduct_reactive_table_data(),
         editable = TRUE,
         rownames = FALSE,
+        escape = FALSE,
         options = list(
           columnDefs = list(
+            list(
+              targets = 2,  # DLT column index (0-based)
+              render = JS(
+                "function(data, type, row, meta) {",
+                "  if (type === 'display') {",
+                "    return '<input type=\"checkbox\" ' + (data ? 'checked' : '') + '>';",
+                "  }",
+                "  return data;",
+                "}"
+              )
+            ),
             list(className = 'dt-center', targets = "_all")
           )
         )
@@ -90,7 +121,15 @@ con_server <- function(id) {
     observeEvent(input$editable_table_cell_edit, {
       info <- input$editable_table_cell_edit
       data <- conduct_reactive_table_data()
-      data[info$row, info$col + 1] <- info$value
+      col_name <- colnames(data)[info$col + 1]
+
+      # Convert checkbox value to logical
+      if (col_name == "DLT") {
+        data[info$row, col_name] <- tolower(info$value) %in% c("true", "1", "yes", "checked")
+      } else {
+        data[info$row, col_name] <- info$value
+      }
+
       conduct_reactive_table_data(data)
     })
   })
