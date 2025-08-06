09e925f5d64925c1843ba091694f5cdf5cc34b9a
MaddieT <maddie.edwards.t@gmail.com>
2025-08-06 15:45:34 +0100

    added reactive table to results card

--- app_skeleton/pages/con_ui.R
+++ app_skeleton/pages/con_ui.R
@@ -1,28 +1,15 @@
-#library(shiny)
-#library(shiny.semantic)
-library(bslib)
+# Load required libraries
 library(shiny)
-library(htmltools)
-library(plotly)
-library(leaflet)
+library(bslib)
 library(DT)
+library(plotly)
 
-# Define your manual dataset
-manual_data_results <- data.frame(
-  Name = c("Alice", "Bob", "Charlie"),
-  Age = c(29, 34, 41),
-  Role = c("Data Scientist", "Statistician", "Researcher")
-)
-manual_data_overview <- data.frame(
-  Name = c("Jim", "Sid", "James"),
-  Age = c(29, 26, 41),
-  Role = c("Data Scientist", "Statistician", "Researcher")
-)
-
+# Dummy plotly widget
 plotly_widget <- plot_ly(x = diamonds$cut) %>%
   config(displayModeBar = FALSE) %>%
   layout(margin = list(t = 0, b = 0, l = 0, r = 0))
 
+# ---------------- UI MODULE ----------------
 con_ui <- function(id) {
   ns <- NS(id)
   page_sidebar(
@@ -33,46 +20,78 @@ con_ui <- function(id) {
         full_screen = TRUE,
         card_header("Results"),
         card_body(
-            plotly_widget),
-        card_body(
           tagList(
-            DTOutput(ns("manual_table_results")),
-            actionButton(ns("add_cohort"), "Add Cohort")
+            plotly_widget,
+            numericInput(ns("treated_participants_input"), "Number of Treated Participants", value = 3, min = 0),
+            DTOutput(ns("editable_table"))
           )
-        )
-      ),
-      card(
-        full_screen = TRUE,
-        card_header("Overview"),
-        card_body(
-            plotly_widget),
-        card_body(
-            DTOutput(ns("manual_table_overview"))
         )
       )
     ),
-      sidebar = sidebar(
-        radioButtons(
-          inputId = ns("choice"),
+    sidebar = sidebar(
+      radioButtons(
+        inputId = ns("choice"),
         label = "Select which design to update during trial conduct:",
         choices = c("CRM", "3+3", "Other"),
         inline = FALSE
       ),
-    actionButton(ns("update_design"), "Update Design"),
-    fileInput(ns("file_upload"), "Upload Previous Responses:", accept = c(".csv", ".rds")),
-    downloadButton(ns("save_button"), "Save Responses"),
-    actionButton(ns("trial_design_share"), "Use Trial Design Input")
+      actionButton(ns("update_design"), "Update Design"),
+      fileInput(ns("file_upload"), "Upload Previous Responses:", accept = c(".csv", ".rds")),
+      downloadButton(ns("con_save_button"), "Save Responses"),
+      actionButton(ns("trial_design_share"), "Use Trial Design Input")
     )
   )
 }
 
+# ---------------- SERVER MODULE ----------------
 con_server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$manual_table_results <- renderDT({
-      datatable(manual_data_results)
+    ns <- session$ns
+
+    # Reactive table data
+    conduct_reactive_table_data <- reactiveVal(
+      data.frame(matrix(ncol = 4, nrow = 0, dimnames = list(NULL, c("No.number", "Dose_level", "DLT", "Posterior_DLT"))))
+    )
+
+    # Update table based on numeric input
+    observe({
+      target_rows <- input$treated_participants_input
+      current_data <- conduct_reactive_table_data()
+      current_rows <- nrow(current_data)
+      rows_to_add <- target_rows - current_rows
+
+      if (rows_to_add > 0) {
+        new_rows <- data.frame(
+          matrix(0, ncol = 4, nrow = rows_to_add, dimnames = list(NULL, c("No.Cohort", "Dose_Level", "DLT", "Posterior_DLT")))
+        )
+        updated_data <- rbind(current_data, new_rows)
+      } else {
+        updated_data <- head(current_data, target_rows)
+      }
+
+      conduct_reactive_table_data(updated_data)
     })
-    output$manual_table_overview <- renderDT({
-      datatable(manual_data_overview)
+
+    # Render editable DT table
+    output$editable_table <- renderDT({
+      datatable(
+        conduct_reactive_table_data(),
+        editable = TRUE,
+        rownames = FALSE,
+        options = list(
+          columnDefs = list(
+            list(className = 'dt-center', targets = "_all")
+          )
+        )
+      )
+    }, server = FALSE)
+
+    # Handle cell edits
+    observeEvent(input$editable_table_cell_edit, {
+      info <- input$editable_table_cell_edit
+      data <- conduct_reactive_table_data()
+      data[info$row, info$col + 1] <- info$value
+      conduct_reactive_table_data(data)
     })
   })
 }
